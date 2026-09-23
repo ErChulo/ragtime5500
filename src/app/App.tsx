9ac05c25d48c19230ab84f8f2bba30d657551738
lopez.cardona <45464652+ErChulo@users.noreply.github.com>
2026-09-23 12:03:15 -0400

    feat: surface integrity diagnostics and audit history

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -9,6 +9,8 @@ import { MatchReview } from '../components/MatchReview';
 import { ExtractionReview } from '../components/ExtractionReview';
 import { AppNavigation, appSections } from '../components/AppNavigation';
 import { BackupRestorePanel } from '../components/BackupRestorePanel';
+import { SystemDiagnosticsPanel } from '../components/SystemDiagnosticsPanel';
+import { AuditLogPanel } from '../components/AuditLogPanel';
 import { DatabaseHealthPanel } from '../components/DatabaseHealthPanel';
 import { AuditHistoryPanel } from '../components/AuditHistoryPanel';
 import { DocumentSearchPanel } from '../components/DocumentSearchPanel';
